429a49792ddf348c51cb8a59f79594e8090dd2d9
Sergey Shuchkin <sergey.shuchkin@gmail.com>
2016-12-02 16:44:09 +0600

    Bash

---
+++ Bash/sendsms.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Отправка SMS через шлюз SMSPILOT.RU на Bash
+#
+# ./sendsms.sh "test" "79087964781"
+#
+text=$1
+phone=$2
+
+# (!!!) Замените XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ
+# на свой API-ключ: https://www.smspilot.ru/my-settings.php#api
+
+apikey=XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ
+
+curl -v --data-urlencode send="$text" --data-urlencode to="$phone" --data apikey="$apikey" http://smspilot.ru/api.php
+
+# альтернативный вариант это запрос через wget, замените XYZ на свой API-ключ
+# wget -q http://smspilot.ru/api.php?send=Hello&to=79087964781&apikey=XYZ"
