858899b152c653ef0ca643397892ae6100e575e2
Sergey Shuchkin <sergey.shuchkin@gmail.com>
2017-08-19 19:52:44 +0600

    default GET

--- PHP/hlr_callback.php
+++ PHP/hlr_callback.php
@@ -2,4 +2,4 @@
 // hlr_callback.php
 // Сюда приходит статус HLR-запроса
 // Пишем данные в лог, поэтому создайте hlr.log в папке скрипта и дайте права на запись
-file_put_contents( __DIR__ . '/hlr.log', "\r\n_POST = ".print_r( $_POST, true), FILE_APPEND );
+file_put_contents( __DIR__ . '/hlr.log', "\r\n_GET = ".print_r( $_GET, true), FILE_APPEND );
